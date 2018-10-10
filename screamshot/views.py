c51369bd615fdae233ed136561714fb6a1e9a9f6
madewulf <madewulf@multitasked.net>
2018-10-10 17:47:25 +0200

    Adding some file caching to screamshot

--- screamshot/views.py
+++ screamshot/views.py
@@ -1,10 +1,13 @@
 import base64
 import logging
 from io import BytesIO
+import os.path
 
 from django.core.urlresolvers import NoReverseMatch
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils.translation import ugettext as _
+import hashlib
+
 
 from .utils import (casperjs_capture, CaptureError, UnsupportedImageFormat,
                     image_mimetype, parse_url)
@@ -16,16 +19,6 @@ def capture(request):
     # Merge both QueryDict into dict
     parameters = dict([(k, v) for k, v in request.GET.items()])
     parameters.update(dict([(k, v) for k, v in request.POST.items()]))
-
-    url = parameters.get('url')
-    if not url:
-        return HttpResponseBadRequest(_('Missing url parameter'))
-    try:
-        url = parse_url(request, url)
-    except NoReverseMatch:
-        error_msg = _("URL '%s' invalid (could not reverse)") % url
-        return HttpResponseBadRequest(error_msg)
-
     method = parameters.get('method', request.method)
     selector = parameters.get('selector')
     data = parameters.get('data')
@@ -34,6 +27,26 @@ def capture(request):
     render = parameters.get('render', 'png')
     size = parameters.get('size')
     crop = parameters.get('crop')
+
+    url = parameters.get('url')
+    m = hashlib.sha256()
+    m.update(str(url).encode('utf-8'))
+    url_md5 = m.hexdigest()
+    media_path = 'media/%s.png' % url_md5
+    if os.path.isfile(media_path):
+        f = open(media_path, 'rb')
+        response = HttpResponse(content_type=image_mimetype(render))
+        response.write(f.read())
+        return response
+    if not url:
+        return HttpResponseBadRequest(_('Missing url parameter'))
+    try:
+        url = parse_url(request, url)
+    except NoReverseMatch:
+        error_msg = _("URL '%s' invalid (could not reverse)") % url
+        return HttpResponseBadRequest(error_msg)
+
+
 
     try:
         width = int(parameters.get('width', ''))
@@ -51,10 +64,10 @@ def capture(request):
                          size=size, waitfor=waitfor, crop=crop, render=render,
                          wait=wait)
     except CaptureError as e:
-        return HttpResponseServerError(e)
+        return HttpResponseBadRequest(e)
     except ImportError:
         error_msg = _('Resize not supported (PIL not available)')
-        return HttpResponseServerError(error_msg)
+        return HttpResponseBadRequest(error_msg)
     except UnsupportedImageFormat:
         error_msg = _('Unsupported image format: %s' % render)
         return HttpResponseBadRequest(error_msg)
@@ -66,6 +79,9 @@ def capture(request):
                 """ % base64.encodestring(stream.getvalue())
         response.write(body)
     else:
+        f = open(media_path, 'wb')
+        f.write(stream.getvalue())
+        f.close()
         response = HttpResponse(content_type=image_mimetype(render))
         response.write(stream.getvalue())
 
